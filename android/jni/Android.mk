7708bdb0367914b958979a2be0707de595254db3
hgl888 <hgl868@126.com>
2016-06-29 10:15:24 +0800

    添加android工程

---
+++ android/jni/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+include ../native/jni/OpenCV.mk
+
+LOCAL_SRC_FILES  := DetectionBasedTracker_jni.cpp
+LOCAL_C_INCLUDES += $(LOCAL_PATH)
+LOCAL_LDLIBS     += -llog -ldl
+
+LOCAL_MODULE     := detection_based_tracker
+
+include $(BUILD_SHARED_LIBRARY)
