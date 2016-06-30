4d2bda9ae2b7200446a6705f915d7b9695f6af0e
hgl888 <hgl868@126.com>
2016-06-30 11:10:40 +0800

    修改代码

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -1,7 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-include ../native/jni/OpenCV.mk
+
+$(call import-add-path,$(LOCAL_PATH)/../../modules)
+
+#include ../native/jni/OpenCV.mk
 
 LOCAL_SRC_FILES  := DetectionBasedTracker_jni.cpp
 LOCAL_C_INCLUDES += $(LOCAL_PATH)
@@ -11,6 +14,7 @@ LOCAL_MODULE     := facedetector
 #LOCAL_SRC_FILES :=  libopencv_java.so
 #LOCAL_MODULE := opencv_java
 
-
-
 include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,.)
+
